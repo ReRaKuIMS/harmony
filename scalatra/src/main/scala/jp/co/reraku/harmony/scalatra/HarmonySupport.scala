f796d5d37d8e9104b51a322bbf0cdc4ac6220efb
Takahiro Kondo <heartery@gmail.com>
2017-04-21 19:07:04 +0900

    Support fileParams and fileMultiParams

--- scalatra/src/main/scala/jp/co/reraku/harmony/scalatra/HarmonySupport.scala
+++ scalatra/src/main/scala/jp/co/reraku/harmony/scalatra/HarmonySupport.scala
@@ -5,7 +5,7 @@ import jp.co.reraku.harmony.RequestContext
 import org.scalatra.{ ScalatraBase, RouteTransformer }
 
 trait HarmonySupport[S <: ScalatraBase, X <: RequestContext] { self: S =>
-  def context(): X
+  def context: X
 
   def arrive(transformers: RouteTransformer*): ScalatraPath[S, X] =
     new ScalatraPath(this, transformers, context)
